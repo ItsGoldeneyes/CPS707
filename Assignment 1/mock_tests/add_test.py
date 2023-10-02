2b2e19e730cb973fab3498dd8ffa4b064d8d2ce5
Thorley <thorley.eric@gmail.com>
2023-10-02 14:33:37 -0400

    rewrote as pseudocode

--- Assignment 1/mock_tests/add_test.py
+++ Assignment 1/mock_tests/add_test.py
@@ -1,42 +1,53 @@
 import unittest
-import ticket
-import eventslog
+
+#note the tests are pseudocoded
 
 class TestAdd(unittest.TestCase):
     #check that input add bring correct prompt
     def add_input_test(self):
-        result = ticket.add()
-        self.assertEqual(result, "Enter Event Number")
+        #input should be the prompt "add" from the user in command line
+        #the result should be the system advancing to the next step of add functionality 
+        #output should be a prompt from the SYSTEM to enter eventID
+        #test should pass if system advanced to next step
     
     #check that inputing event ID prompts for quantity
     def add_event_test(self):
-        result = ticket.addEvent(eventID)
-        self.assertEqual(result, "Enter Ticket Quantity")
+        #input from user will be an event id
+        #result should be checking the event ID against the system
+        #system should then move to next step of add functionalty by prompting user for quantity
+        #output is system moving to next step and prompting user for quantity
+        #test should pass if eventID matches one in the system
     
     #check tickets are added to event
     def add_quantity_test(self):
-        result = ticket.addEvent(eventID).addQuantity(quantity)
-        self.assertEqual(result, (quantity, "tickets added to event ", eventID))
+        #input from user will be quantity to be added and event ID
+        #output will be successfully adding tickets to the backend
+        #output to user will be SUCCESS notification
+        #test will pass if backend is updated
 
     #check the event log has been updated
     def add_eventlog_test(self):
-        result = ticket.addEvent(eventID).addQuantity(quantity).assertLogs()
-        self.assertIsInstance(result, eventslog)
+        #input will be from function to write to the event log 
+        #output will be to confirm the transaction has been recorded
+        #test will pass if transaction records to event log
 
     #test for bad event ID
     def add_badeventname_test(self):
-        eventID = "BADINPUT"
-        result = ticket.addEvent(eventID)
-        self.assertEqual(result, "Invalid Event ID")
+        #input will be to provide a badinput (nonmatching event id) to the add function
+        #output will be a message that event ID is invalid
+        #test should pass if system can not find ID and uses case 
+        #to prompt the user for correct info 
 
     #try bad input for quantity
     def add_badinput_test(self):
-        quantity = -3
-        result = ticket.addEvent(eventID).addQuantity(quantity)
-        self.assertEqual(result, "Invalid Quantity entered")
+        #input will be a nonpositive integer as an amount to add to an event
+        #ex -3
+        #output will be a failstate in the backend and prompt to user to enter a valid quantity
+        #test will Pass if failstate met
 
     # test adding without admin fails
     #assuming not logged in
     def add_nologin_test(self):
-        result = ticket.add()
-        self.assertEqual(result, "Must be Admin to Add")
+        #input will be an "add" prompt from a non admin account state
+        #output wil be a failstate that prompts user about admin status
+        #test will pass if proper failstate is reached
--- Assignment 1/mock_tests/delete_test.py
+++ Assignment 1/mock_tests/delete_test.py
@@ -1,56 +1,65 @@
 import unittest
-import ticket
-import eventslog
 
+#note tests are psuedocoded
 #assume login as admin unless specified 
 class TestDelete(unittest.TestCase):
 
     #tests that inputing delete will bring up prompt
     def delete_input_test(self):
-        input = ticket.delete()
-        self.assertEqual(input, "Enter Event Number")
+        #input should be the prompt "delete" from the user in command line
+        #the result should be the system advancing to the next step of delete functionality 
+        #output should be a prompt from the SYSTEM to enter event ID
+        #test should pass if system advanced to next step of delete process
 
     #test deleting a quantity for a valid event
     def delete_tickets_test(self):
-        input = ticket.deleteEvent(eventID)
-        self.assertEqual(input, "Enter Ticket Quantity")
+        #input from user will be an event id and quantity
+        #result should be checking the event ID against the system
+        #system should then delete the quantity of tickets from event
+        #output is system moving to next step and prompting user for quantity
+        #test should pass if eventID matches one in the system
 
     #test deleting the actual event
     def delete_event_test(self):
-        input = ticket.deleteEvent(eventID).deleteQuantity("ALL")
-        output = (eventID, " Deleted")
-        self.assertEqual(input, output)
+        #input from user will be keyword like "ALL" for delete processs
+        #system should then delete the entire event
+        #output is system deleting event and confirming with USER
+        #test should pass if eventID matches one in the system
 
     #test the deletion quantity is being correctly captured in eventslog
     def delete_ticket_transaction_test(self):
-        input = ticket.deleteEvent(eventID).deleteQuantity(quantity).assertLogs()
-        self.assertIn(input, eventslog)
+        #input will be from a function to delete a given quantity and write to the event log 
+        #output will be to confirm the transaction has been recorded
+        #test will pass if transaction records to event log
 
     #test deleting entire event is captured in events log
     def delete_event_transaction_test(self):
-        input = ticket.deleteEvent(eventID).deleteQuantity("ALL").assertLogs()
-        self.assertIn(input, eventslog)   
+        #input will be from a function to delete an event and record to the event log
+        #output will be to confirm the transaction has been recorded
+        #test will pass if transaction records to event log 
 
     #test delete without admin priveledges
     def delete_noadmin_test(self):
-        input = ticket.delete()
-        self.assertEqual(input, "Must be Admin to Delete")
+        #input will be an "delete" prompt from a non admin account state
+        #output wil be a failstate that prompts user about admin status
+        #test will pass if poper failstate is reached
 
     #test bad event ID will fail
     def delete_wrongevent_test(self):
-        eventID = "BADINPUT"
-        input = ticket.deleteEvent(eventID)
-        self.assertEqual(input, "Invalid event ID")
+        #input will be an invalid eventID to delete
+        #ex 999999999
+        #output will be a failstate in the backend and prompt to user to enter a valid quantity
+        #test will Pass if failstate met
     
     #test bad quantity input will fail
     def delete_wrongticket_test(self):
-        quantity = -3
-        input = ticket.deleteEvent(eventID).deleteQuantity(quantity)
-        self.assertEqual(input, "Invalid Quantity Entered")
+        #input will be a nonpositive integer as an amount to delete from an event
+        #ex -3
+        #output will be a failstate in the backend and prompt to user to enter a valid quantity
+        #test will Pass if failstate met
     
     #test you cant delete more than number of available tickets
     def delete_toomany(self):
-        max_number = ticket.event(eventID).quantity()
-        quantity = max_number + 1
-        input =  ticket.deleteEvent(eventID).deleteQuantity(quantity)
-        self.assertEqual(input, "Quantity is more than Max")
+        #input will be a quantity greater than the max number of tickets available to the delete function
+        #output will be a failstate and prompt to user that quantity is greater than max
+        #test will pass if failstate is reached
