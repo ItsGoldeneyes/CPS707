8fcf32f4b4e3be0ffb7f8708a8dc65cc73cd9026
Adam <97778776+ItsGoldeneyes@users.noreply.github.com>
2023-10-20 19:05:16 -0400

    Summary (required)

--- Assignment 2/frontend.py
+++ Assignment 2/frontend.py
@@ -1,23 +1,19 @@
 import logging
 from backend import Backend
 
-RUN = True
-LOGIN_TYPE = None
-
-'''
-Configure Logging
-'''
-logging.basicConfig(filename='daily.log',
-                     level=logging.INFO,
-                     format= "%(message)s")
-
-
 class Frontend:
     def __init__(self):
         self.privilege = None
         self.run = True
         self.transaction_count=0
+        self.current_events = []
         self.backend = Backend()
+        
+        # Configure logging
+        logging.basicConfig(filename='daily.log',
+                     level=logging.INFO,
+                     format= "%(message)s")
+        
     
     def login(self):
         '''
@@ -32,66 +28,14 @@ class Frontend:
             else:
                 print("Invalid session type. Please ender admin or sales")
         
-        # Import current events file
-
-    def create_event(self):
-        event_name = input("Enter event name: ")
-        event_date = input("Enter event date (YYYYMMDD): ")
-        num_tickets = int(input("Enter number of tickets available: "))
-        # Call the backend function to create the event
-        result = self.backend.create_event(event_name, event_date, num_tickets)
-        if result:
-            print("Event created successfully.")
-        else:
-            print("Failed to create the event.")
-
-    def delete(self):
-        if not self.privilege:
-            print("You must be logged in as an admin to delete tickets from an event.")
-            return
-
-        event_name = input("Enter the name of the event: ")
-        ticket_number = int(input("Enter the ticket number to delete: "))
-
-        if not self.backend.event_exists(event_name):
-            print("Event not found or cannot delete tickets from it.")
-            return
-
-        if not self.backend.ticket_exists(event_name, ticket_number):
-            print("Ticket not found or cannot be deleted.")
-            return
-
-        confirmation = input(f"Are you sure you want to delete ticket {ticket_number} from the event '{event_name}'? (yes/no): ").lower()
-
-        if confirmation == 'yes':
-            result = self.backend.delete_ticket(event_name,ticket_number)
-
-            if result:
-                print(f"Ticket: {ticket_number} deleted successfully from event {event_name}")
-            else:
-                print(f"Failed to delete Ticket: {ticket_number} successfully from event {event_name}")
-
-        else:
-            print("Ticket deletion canceled")
-
-    def sell(self):
-        event_name = input("Enter the name of the event: ")
-        quantity = int(input("Enter the number of tickets to sell: "))
-
-        payment_method = input("Enter payment method: ")
-        card_number = input("Enter card number: ")
-
-        customer_name = input("Enter customer name: ")
-        customer_email = input("Enter customer email: ")
-
-        sale_result = self.backend.sell_tickets(event_name, quantity, payment_method, card_number, customer_name,
-                                                customer_email)
-
-        if sale_result["status"] == "success":
-            print(f"Sale Successful!")
-        else:
-            print("Sale failed")
-
+        # Verify current events file and import it
+        with open("current_events.txt", "r") as f:
+            current_events = f.readlines()
+            if current_events:
+                self.current_events = current_events
+                # self.backend.import_events(current_events)
+    
+    
     def logout(self):
         '''
         Logout of current session
@@ -100,6 +44,86 @@ class Frontend:
             print("You are not logged in")
             return
         self.privilege = None
+             
+        
+    def create(self):
+        '''
+        Create a new event
+        '''
+        valid_input = False
+        # Test for valid input
+        while not valid_input:
+            event_name = input("Enter event name: ")
+            event_date = input("Enter event date (YYYYMMDD): ")
+            num_tickets = int(input("Enter number of tickets available: "))
+            valid_input = True
+        # Call the backend function to create the event
+        # result = self.backend.create_event(event_name, event_date, num_tickets)
+        result = True
+        if result:
+            print("Event created successfully.")
+        else:
+            print("Failed to create the event.")
+
+
+    def delete(self):
+        '''
+        Delete ticket from event
+        '''
+        valid_input = False
+        # Test for valid input
+        while not valid_input:
+            event_name = input("Enter the name of the event: ")
+            ticket_number = int(input("Enter the ticket number to delete: "))
+            valid_input = True
+
+        # if not self.backend.event_exists(event_name):
+        #     print("Event not found or cannot delete tickets from it.")
+        #     return
+
+        # if not self.backend.ticket_exists(event_name, ticket_number):
+        #     print("Ticket not found or cannot be deleted.")
+        #     return
+
+        confirmation = input(f"Are you sure you want to delete ticket {ticket_number} from the event '{event_name}'? (yes/no): ").lower()
+
+        if confirmation == 'yes':
+            # result = self.backend.delete_ticket(event_name,ticket_number)
+            result = True
+
+            if result:
+                print(f"Ticket: {ticket_number} deleted successfully from event {event_name}")
+            else:
+                print(f"Failed to delete Ticket: {ticket_number} successfully from event {event_name}")
+        else:
+            print("Ticket deletion canceled")
+
+
+    def sell(self):
+        '''
+        Sell tickets to customer
+        '''
+        valid_input = False
+        # Test for valid input
+        while not valid_input:
+            event_name = input("Enter the name of the event: ")
+            quantity = int(input("Enter the number of tickets to sell: "))
+
+            payment_method = input("Enter payment method: ")
+            card_number = input("Enter card number: ")
+
+            customer_name = input("Enter customer name: ")
+            customer_email = input("Enter customer email: ")
+            valid_input = True
+
+        # sale_result = self.backend.sell_tickets(event_name, quantity, payment_method, card_number, customer_name,
+                                                # customer_email)
+        sale_result = {"status": "success"}
+
+        if sale_result["status"] == "success":
+            print(f"Sale Successful!")
+        else:
+            print("Sale failed")
         
         
     def add(self):
@@ -137,19 +161,25 @@ class Frontend:
         return event_transaction
     
     def help(self):
-        #options for admin user
+        '''
+        Displays a help menu
+        '''
+        # Options for an admin user
         if (self.privilege == "admin"):
-            print(
-                "Enter: 'create' for new event | 'add' to add tickets | " +
-                    "'delete' to remove events or tickets |" +
-                    "'sell' if tickets have been sold to customer | 'return' to refund sold tickets |" +
-                    "'logout' to logout |'exit' to exit"
+            print("\t" +
+                    "'create' for new event \n\t" + 
+                    "'add' to add tickets \n\t" +
+                    "'delete' to remove events or tickets \n\t" +
+                    "'sell' if tickets have been sold to customer \n\t"  + 
+                    "'return' to refund sold tickets \n\t" +
+                    "'logout' to logout \n\t" +
+                    "'exit' to exit"
                 )
-        #options for sales user 
+        # Options for a sales user 
         elif (self.privilege == "sales"):
-            print(
-                "Enter: " +
-                    "'sell' if tickets have been sold to customer | 'return' to refund sold tickets |" +
+            print( "\t"+
+                    "'sell' if tickets have been sold to customer \n\t" + 
+                    "'return' to refund sold tickets \n\t" +
                     "'logout' to logout |'exit' to exit"
                 )   
         
@@ -175,7 +205,7 @@ class Frontend:
                 case "add":
                     self.add()
                 case "create":
-                    self.create_event()
+                    self.create()
                 case "delete":
                     self.delete()
                 case "sell":
